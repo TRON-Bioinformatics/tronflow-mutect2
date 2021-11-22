1b08a476d02b99e2e5001b31d46b87a2ac4cfa79
Pablo Riesgo Ferreiro <priesgoferreiro@gmail.com>
2021-11-22 10:34:30 +0100

    fix test

--- tests/test_03.sh
+++ tests/test_03.sh
@@ -7,4 +7,4 @@ output=output/test3
 echo -e "sample_name_with_replicates\t"`pwd`"/test_data/TESTX_S1_L001.bam,"`pwd`"/test_data/TESTX_S1_L001.bam\t"`pwd`"/test_data/TESTX_S1_L002.bam,"`pwd`"/test_data/TESTX_S1_L002.bam" > test_data/test_input_with_replicates.txt
 nextflow main.nf -profile test,conda --input_files test_data/test_input_with_replicates.txt --output $output
 
-test -s $output/sample_name/sample_name.mutect2.vcf || { echo "Missing output VCF file!"; exit 1; }
+test -s $output/sample_name_with_replicates/sample_name_with_replicates.mutect2.vcf || { echo "Missing output VCF file!"; exit 1; }
