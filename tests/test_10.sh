2829297723cd34cea2042e9a544267e145bf7802
Luis Kress <luis.kress@tron-mainz.de>
2024-06-14 08:12:10 +0200

    Separate CI nexflow profile from test profile

--- tests/test_10.sh
+++ tests/test_10.sh
@@ -4,7 +4,7 @@
 source bin/assert.sh
 output=output/test10
 
-nextflow main.nf -profile test,conda --output $output \
+nextflow main.nf -profile test,conda,ci --output $output \
 --input_name sample_name \
 --input_tumor_bam `pwd`/test_data/SRR8244887.preprocessed.downsampled.bam \
 --input_normal_bam `pwd`/test_data/SRR8244836.preprocessed.downsampled.bam
